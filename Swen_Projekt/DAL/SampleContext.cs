31cf08c31ac151de2f9ba30745116fd949cbaf04
alan <alanpisi@gmail.com>
2023-04-04 15:15:20 +0200

    new project first commit

---
+++ Swen_Projekt/DAL/SampleContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace DAL
+{
+	public class SampleContext : DbContext
+	{
+		public DbSet<User> Users { get; set; }
+	}
+}
---
+++ Swen_Projekt/DAL/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DAL
+{
+	public class User
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public string Password { get; set; }
+
+	}
+}
